75cb9d7248612920b021425bb4e7bcf878d104dc
Sangam Arora <arorasam2109@gmail.com>
2024-07-06 16:26:32 +0530

    added onrender url

--- backend/middleware/checkDomain.js
+++ backend/middleware/checkDomain.js
@@ -3,11 +3,12 @@
 function checkDomain(req, res, next) {
     const allowedCollegeDomain = 'gne1.gndec.ac.in';
     const allowedVercelDomain ="training-tracker-gndec.vercel.app";
+    const allowedOnRenderDomain = "training-tracker-cse.onrender.com";
     const origin = req.get('Origin') || req.get('Referer');
 
     if (origin) {
         const url = new URL(origin);
-        if (url.hostname === allowedCollegeDomain || url.hostname === allowedVercelDomain) {
+        if (url.hostname === allowedCollegeDomain || url.hostname === allowedVercelDomain || url.hostname === allowedOnRenderDomain) {
             return next();
         }
     }
