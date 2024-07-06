8ffc30220f43d6a7a53f5bcf283f2123e05c1962
Sangam Arora <arorasam2109@gmail.com>
2024-07-06 16:49:09 +0530

    corrected checkDomain file

--- backend/middleware/checkDomain.js
+++ backend/middleware/checkDomain.js
@@ -4,7 +4,11 @@ function checkDomain(req, res, next) {
     const allowedCollegeDomain = 'gne1.gndec.ac.in';
     const allowedVercelDomain ="training-tracker-gndec.vercel.app";
     const origin = req.get('Origin') || req.get('Referer');
+    const publicUrl = '/api/certificate/';
 
+    if (req.url.startsWith(publicUrl)) {
+        return next();
+    }
     if (origin) {
         const url = new URL(origin);
         if (url.hostname === allowedCollegeDomain || url.hostname === allowedVercelDomain) {
